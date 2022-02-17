11f242c8b127c2baf5cc26aa0ff6dfa68ee492b1
Márcio Santos Gacilin <marcio.gacilin@imaginebeyond.com.br>
2022-02-16 22:55:00 -0300

    lógica de acesso e login

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -11,8 +11,17 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         opacity: 1
       })),
       transition('void => criado', [
-        style({ opacity: 0 }),
-        animate('500ms 1s ease-in-out')
+        style({ opacity: 0, transform: 'translate(-50px, 0)' }),
+        animate('0ms 1s ease-in-out')
+      ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(50px, 0)' }),
+        animate('0ms 1s ease-in-out')
       ])
     ])
   ]
@@ -20,10 +29,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 export class AcessoComponent implements OnInit {
 
   public estadoBanner: string = 'criado'
+  public estadoPainel: string = 'criado'
+  public cadastro: boolean = false
   
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public exibirPainel(event: string): void {
+    this.cadastro = event === 'cadastro' ? true : false
+  }
 }
